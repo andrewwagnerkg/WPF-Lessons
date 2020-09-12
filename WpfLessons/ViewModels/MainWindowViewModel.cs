2942653e3f790fa30a9e2890dc03e00f83535d1c
Andrew Wagner <38209339+andrewwagnerkg@users.noreply.github.com>
2020-09-13 00:29:53 +0600

    View model is avaliable

--- WpfLessons/ViewModels/MainWindowViewModel.cs
+++ WpfLessons/ViewModels/MainWindowViewModel.cs
@@ -4,6 +4,14 @@ namespace WpfLessons.ViewModels
 {
     internal class MainWindowViewModel : ViewModel
     {
+        private string _Title="Test";
+        public string Title { get=>_Title; set=>Set(ref _Title,value); }
+
+        private string _Status;
+        public string Status{get => _Status; set => Set(ref _Status, value);}
+
+        private double _Progress;
+        public double Propgress { get=>_Progress; set=>Set(ref _Progress,value); }
 
     }
 }
