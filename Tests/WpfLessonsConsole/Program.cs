791fbc02d4f3bfd832a83f2889afeb46a3166238
Andrew Wagner <38209339+andrewwagnerkg@users.noreply.github.com>
2020-09-13 12:33:17 +0600

    Getted data from server

--- Tests/WpfLessonsConsole/Program.cs
+++ Tests/WpfLessonsConsole/Program.cs
@@ -1,12 +1,48 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace WpfLessonsConsole
 {
     static class Program
     {
+        private const string DataUrl = @"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
+
+        public static async Task<Stream> GetDataStream()
+        {
+            using var httpClient = new HttpClient();
+            var response = await httpClient.GetAsync(DataUrl, HttpCompletionOption.ResponseHeadersRead);
+            return await response.Content.ReadAsStreamAsync();
+        }
+
+        public static IEnumerable<string> GetData()
+        {
+            using var dataStream = GetDataStream().Result;
+            using var dataReader = new StreamReader(dataStream);
+            while (!dataReader.EndOfStream)
+            {
+                var line = dataReader.ReadLine();
+                if (string.IsNullOrWhiteSpace(line)) continue;
+                yield return line;
+            }
+        }
+
+        private static DateTime[] GetDates() => GetData()
+            .First()
+            .Split(',')
+            .Skip(4)
+            .Select(s => DateTime.Parse(s, CultureInfo.InvariantCulture))
+            .ToArray();
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Console.WriteLine(String.Join("\r\n", GetDates()));
+            Console.ReadLine();
         }
     }
 }
