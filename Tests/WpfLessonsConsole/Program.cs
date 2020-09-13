962cf09ebbd72a5c81470892afb20fb7598a91cb
Andrew Wagner <38209339+andrewwagnerkg@users.noreply.github.com>
2020-09-13 13:03:55 +0600

    Buisnes logic test

--- Tests/WpfLessonsConsole/Program.cs
+++ Tests/WpfLessonsConsole/Program.cs
@@ -19,7 +19,7 @@ namespace WpfLessonsConsole
             return await response.Content.ReadAsStreamAsync();
         }
 
-        public static IEnumerable<string> GetData()
+        public static IEnumerable<string> GetDataLines()
         {
             using var dataStream = GetDataStream().Result;
             using var dataReader = new StreamReader(dataStream);
@@ -31,17 +31,31 @@ namespace WpfLessonsConsole
             }
         }
 
-        private static DateTime[] GetDates() => GetData()
+        private static DateTime[] GetDates() => GetDataLines()
             .First()
             .Split(',')
             .Skip(4)
             .Select(s => DateTime.Parse(s, CultureInfo.InvariantCulture))
             .ToArray();
 
+        private static IEnumerable<(string Country, string Province, int[]Count)> GetData()
+        {
+            var lines = GetDataLines()
+                .Skip(1)
+                .Select(line => line.Split(','));
+            foreach (var row in lines)
+            {
+                var Province = row[0].Trim();
+                var Country = row[1].Trim(' ', '"').Trim(' ',',');
+                var Count = row.Skip(5).Select(int.Parse).ToArray();
+                yield return (Country, Province, Count);
+            }
+        }
         static void Main(string[] args)
         {
 
-            Console.WriteLine(String.Join("\r\n", GetDates()));
+            var russia = GetData().First(v => v.Country .Equals("Russia", StringComparison.OrdinalIgnoreCase));
+            Console.WriteLine(String.Join("\r\n",GetDates().Zip(russia.Count,(date,count)=>$"{date:yyyy-MM-dd} - {count}")));
             Console.ReadLine();
         }
     }
