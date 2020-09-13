b4055351c885a030c0bf29704b2ba38c3ba291ea
Andrew Wagner <38209339+andrewwagnerkg@users.noreply.github.com>
2020-09-13 13:46:44 +0600

    AddClassess

--- WpfLessons/ViewModels/MainWindowViewModel.cs
+++ WpfLessons/ViewModels/MainWindowViewModel.cs
@@ -1,9 +1,24 @@
-﻿using WpfLessons.ViewModels.Base;
+﻿using System;
+using System.Collections.Generic;
+using WpfLessons.ViewModels.Base;
+using WpfLessons.Models;
 
 namespace WpfLessons.ViewModels
 {
     internal class MainWindowViewModel : ViewModel
     {
+        public MainWindowViewModel()
+        {
+            var data_points=new List<DataPoint>((int)(360/0.1));
+            for (var x = 0d; x <= 360; x+=0.1)
+            {
+                var to_rad = Math.PI / 180;
+                var y = Math.Sin(x * to_rad);
+                data_points.Add(new DataPoint{XValue = x, YValue = y});
+            }
+
+            TestData = data_points;
+        }
         private string _Title="Test";
         public string Title { get=>_Title; set=>Set(ref _Title,value); }
 
@@ -13,5 +28,7 @@ namespace WpfLessons.ViewModels
         private double _Progress;
         public double Progress { get=>_Progress; set=>Set(ref _Progress,value); }
 
+        private IEnumerable<DataPoint> _TestData;
+        public IEnumerable<DataPoint> TestData { get=>_TestData; set=>Set(ref _TestData,value); }
     }
 }
