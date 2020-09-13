8d4a2fc91bab5f925327710ee0da12a790302f68
Andrew Wagner <38209339+andrewwagnerkg@users.noreply.github.com>
2020-09-13 10:03:15 +0600

    Create close application command

---
+++ WpfLessons/Infrastructure/Commands/Commons/CloseApplicationCommand.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+using WpfLessons.Infrastructure.Commands.Base;
+
+namespace WpfLessons.Infrastructure.Commands.Commons
+{
+
+    internal class CloseApplicationCommand:BaseCommand
+    {
+        public override bool CanExecute(object parameter) => true;
+        public override void Execute(object parameter) => Application.Current.Shutdown();
+    }
+}
